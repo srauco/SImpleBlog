faf976731849f7fc9e2777f26f84e0a5c18113bd
Steve Rauco <github@rauco.org>
2016-03-10 13:45:29 -0500

    Added basic login form

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
@@ -11,6 +12,20 @@ namespace SimpleBlog.Controllers
         public ActionResult Login()
         {
             return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+            if (form.Username != "rainbow")
+            {
+                ModelState.AddModelError("Username", "Incorrect Username");
+                return View(form);
+            }
+            return Content("Form is valid");
         }
     }
 }
