ac5bcb698cafc59eb825b4ad7db7e5d42ea5bee0
Steve Rauco <github@rauco.org>
2016-03-17 15:23:40 -0400

    Imported styes and scripts, Jquery and bootstrap

--- SimpleBlog/Global.asax.cs
+++ SimpleBlog/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace SimpleBlog
 {
@@ -13,6 +14,8 @@ namespace SimpleBlog
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
