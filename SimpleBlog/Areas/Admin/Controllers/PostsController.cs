060665edea277983dcfb05bc21c8afc7a442e1d3
Steve Rauco <github@rauco.org>
2016-03-21 16:03:59 -0400

    Put together basic admin layout

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,17 +1,21 @@
-﻿using System;
+﻿using SimpleBlog.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+
+    [SelectedTab("posts")]
     public class PostsController : Controller
     {
         public ActionResult Index()
         {
-            return Content("Admin Posts!");
+            return View();
         }
     }
 }
--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.Infrastructure;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTab("users")]
     public class UsersController : Controller
     {
         public ActionResult Index()
         {
-            return Content("User");
+            return View();
         }
     }
 }
